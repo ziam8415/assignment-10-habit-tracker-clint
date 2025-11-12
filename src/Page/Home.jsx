a4715806434e6c748b3494c9f9d9273074fb541e
ziam123-hub <ziamakhand@gmail.com>
2025-11-12 06:54:27 +0600

    added 6 newest habit in ui

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,49 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AllHabitCard from "../Component/AllHabitCard";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/newestHabits")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
   return (
     <div>
-      {/* carosol */}
-
-      <div className="carousel w-full">
-        <div id="item1" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-            className="w-full"
-          />
+      {/* carosole */}
+      <div>
+        <div className="carousel w-full">
+          <div id="item1" className="carousel-item w-full">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+              className="w-full"
+            />
+          </div>
+          <div id="item2" className="carousel-item w-full">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+              className="w-full"
+            />
+          </div>
+          <div id="item3" className="carousel-item w-full">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+              className="w-full"
+            />
+          </div>
+          <div id="item4" className="carousel-item w-full">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+              className="w-full"
+            />
+          </div>
         </div>
-        <div id="item2" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-            className="w-full"
-          />
-        </div>
-        <div id="item3" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-            className="w-full"
-          />
-        </div>
-        <div id="item4" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-            className="w-full"
-          />
+        <div className="flex w-full justify-center gap-2 py-2">
+          <a href="#item1" className="btn btn-xs">
+            1
+          </a>
+          <a href="#item2" className="btn btn-xs">
+            2
+          </a>
+          <a href="#item3" className="btn btn-xs">
+            3
+          </a>
+          <a href="#item4" className="btn btn-xs">
+            4
+          </a>
         </div>
       </div>
-      <div className="flex w-full justify-center gap-2 py-2">
-        <a href="#item1" className="btn btn-xs">
-          1
-        </a>
-        <a href="#item2" className="btn btn-xs">
-          2
-        </a>
-        <a href="#item3" className="btn btn-xs">
-          3
-        </a>
-        <a href="#item4" className="btn btn-xs">
-          4
-        </a>
+
+      {/* 6 newest habit */}
+      <div className="border mt-20">
+        <h1 className="text-5xl text-center text-gray-500 font-bold">
+          Resent Added Habits
+        </h1>
+        <div className="my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+          {data.map((d) => (
+            <AllHabitCard key={d._id} habit={d}></AllHabitCard>
+          ))}
+        </div>
       </div>
     </div>
   );
